95f9becf7f6fa7fb6be0021bb8a7f4710b1b2d7e
therock ani <anitherock44@gmail.com>
2024-07-27 18:23:42 +0100

    final commit

--- ignition/modules/depoly.js
+++ ignition/modules/depoly.js
@@ -5,10 +5,10 @@ module.exports = buildModule("OnePiecePersonalityDappModule", (m) => {
   const onePiecePersonalityDapp = m.contract("OnePieceMint");
 
   // Log the address where the contract is deployed
-  m.afterDeploy(async () => {
+ /* m.(async () => {
     const address = await onePiecePersonalityDapp.getAddress();
     console.log("OnePiecePersonalityDapp deployed to:", address);
-  });
+  });*/
 
   return { onePiecePersonalityDapp };
 });
